0815fc3d3c4f6622dad549380eb8dd7d5f7c081a
agapoov <nikitosik298765421@gmail.com>
2025-10-15 21:51:35 +0300

    добавил cors, csrf, поправил деплой

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -36,6 +36,24 @@ ALLOWED_HOSTS = [
     "127.0.0.1"
 ]
 
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://smtlog.ru",
+    "https://www.smtlog.ru",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost",
+    "http://127.0.0.1",
+]
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://smtlog.ru",
+    "https://www.smtlog.ru",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 
 # Application definition
 
@@ -49,12 +67,13 @@ INSTALLED_APPS = [
 
     'rest_framework',
 
-
+    'corsheaders',
 
     "orders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,6 +153,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
